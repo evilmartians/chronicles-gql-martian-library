240a663e7bf9ffd6d74e10a788580b68ea7f2b0e
HellSquirrel <zloymult@gmail.com>
2019-06-16 22:38:14 +0300

    Dealing with cache

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,12 +1,14 @@
-import "babel-polyfill";
-import React from "react";
-import { render } from "react-dom";
-import Provider from "../components/Provider";
-import Library from "../components/Library";
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+import Library from '../components/Library';
+import UserInfo from '../components/UserInfo';
 
 render(
   <Provider>
+    <UserInfo />
     <Library />
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
