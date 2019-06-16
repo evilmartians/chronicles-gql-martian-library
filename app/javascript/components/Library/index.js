cf422e15eb57b0d5a8314f1ab2846383b78f755b
HellSquirrel <zloymult@gmail.com>
2019-06-17 00:29:57 +0300

    Updating Library component

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,31 +1,35 @@
-import React from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
+// app/javascript/components/Library
+import React, { useState } from 'react';
+import { Query } from 'react-apollo';
+import { LibraryQuery } from './operations.graphql';
+import cs from './styles';
 
-const LibraryQuery = gql`
-  {
-    items {
-      id
-      title
-      user {
-        email
-      }
-    }
-  }
-`;
+const Library = () => {
+  const [item, setItem] = useState(null);
+  return (
+    <Query query={LibraryQuery}>
+      {({ data, loading }) => (
+        <div className={cs.library}>
+          {loading || !data.items
+            ? 'loading...'
+            : data.items.map(({ title, id, user, imageUrl, description }) => (
+                <button
+                  key={id}
+                  className={cs.plate}
+                  onClick={() => setItem({ title, imageUrl, id, description })}
+                >
+                  <div className={cs.title}>{title}</div>
+                  <div>{description}</div>
+                  {imageUrl && <img src={imageUrl} className={cs.image} />}
+                  {user ? (
+                    <div className={cs.user}>added by {user.email}</div>
+                  ) : null}
+                </button>
+              ))}
+        </div>
+      )}
+    </Query>
+  );
+};
 
-export default () => (
-  <Query query={LibraryQuery}>
-    {({ data, loading }) => (
-      <div>
-        {loading
-          ? "loading..."
-          : data.items.map(({ title, id, user }) => (
-              <div key={id}>
-                <b>{title}</b> {user ? `added by ${user.email}` : null}
-              </div>
-            ))}
-      </div>
-    )}
-  </Query>
-);
+export default Library;
