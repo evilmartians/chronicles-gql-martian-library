e860a1d09d70945850a32361beb449478f2a3c5b
HellSquirrel <zloymult@gmail.com>
2019-06-16 21:56:29 +0300

    House keeping

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+environment.loaders.append('graphql', {
+    test: /\.(graphql|gql)$/,
+    exclude: /node_modules/,
+    loader: 'graphql-tag/loader',
+});
+
+module.exports = environment;
