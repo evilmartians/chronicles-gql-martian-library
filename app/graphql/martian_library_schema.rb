f634afe4401bc989df291f93f36c7b130f5e94cf
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2021-11-19 00:10:44 +0300

    Add subscription use

--- app/graphql/martian_library_schema.rb
+++ app/graphql/martian_library_schema.rb
@@ -1,4 +1,6 @@
 class MartianLibrarySchema < GraphQL::Schema
+  use GraphQL::Subscriptions::ActionCableSubscriptions
+
   mutation(Types::MutationType)
   query(Types::QueryType)
   subscription(Types::SubscriptionType)
