ecd4beb8fbb1afbbeb88b63deb3228dc012c0e84
DmitryTsepelev <dmitry.a.tsepelev@gmail.com>
2021-11-21 21:38:03 +0300

    Do not add methods to subscription

--- app/graphql/types/subscription_type.rb
+++ app/graphql/types/subscription_type.rb
@@ -2,8 +2,5 @@ module Types
   class SubscriptionType < GraphQL::Schema::Object
     field :item_added, Types::ItemType, null: false, description: "An item was added"
     field :item_updated, Types::ItemType, null: false, description: "Existing item was updated"
-
-    def item_added; end
-    def item_updated; end
   end
 end
